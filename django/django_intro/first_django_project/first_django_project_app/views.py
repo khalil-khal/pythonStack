16a0a8748512b14c6d7be428389c51e9d1817814
khalil <johndoe@example.com>
2021-05-19 11:41:23 +0300

    first project

---
+++ django/django_intro/first_django_project/first_django_project_app/views.py
@@ -0,0 +1,31 @@
+
+from django.http.response import JsonResponse
+from django.shortcuts import render, HttpResponse,redirect
+
+# Create your views here.
+
+def index(request):
+    khalil = {
+        "title":"My first Blog",
+        "content":"khalil"
+    }
+    return JsonResponse(khalil)
+    
+def root(request):
+    return  redirect("/blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return  redirect("/")
+
+def show(request,number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request,number):
+    
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request,number):
+    return  redirect("/blogs")
